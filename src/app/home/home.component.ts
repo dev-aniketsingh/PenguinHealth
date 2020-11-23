708e78bcd9f82ab8420d702f6ea08a6abf419c49
David Bouchedid <davidbouchedid@arcusscale.com>
2020-11-23 14:52:44 -0500

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  name : string;
+  ysuID : number;
+  lastAssessment : number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.name = "David Bouchedid";
+    this.lastAssessment = 6;
+    this.ysuID = 78818300;
   }
 
 }
